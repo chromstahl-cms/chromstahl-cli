a90c835868af701fb66cccb2391de3d8117359c0
Lars Grahmann <lars@kloud.software>
2019-04-18 16:33:06 +0200

    add initial cli functionality

---
+++ cli.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+    echo "usage: chrom-cli new NAME"
+    echo "usage: chrom-cli build"
+    exit 1
+fi
+
+if [ $1 = "build" ]
+then
+    if [ ! -f meta.json ]; then
+        echo "meta.json file missing. Not a chromstahl project?"
+        exit 1
+    fi
+
+    NAME=$(cat meta.json | jq -r ".name")
+
+    if [ -d backend ]; then
+        cd backend/ && ./gradlew build && mv build/libs/*.jar ../plugin.jar && cd ../
+    else
+        echo "no backend present, skipping";
+    fi
+
+    if [ -d frontend ]; then
+        cd frontend/ && npm install && npm run build && npm pack && mv *.tgz ../frontend.tgz && cd ../
+    else
+        echo "no frontend present, skipping";
+    fi
+
+    tar -zcvf $NAME.tar.gz meta.json plugin.jar frontend.tgz
+fi
+
+if [ $1 = "new" ]
+then
+    NAME=$2
+    echo $PWD
+
+
+    mkdir test && cd test && git init
+    mkdir frontend/
+    mkdir backend/
+    cat << EOF >> meta.json
+{
+    "name": "$NAME",
+    "author": "example",
+    "version": 1
+}
+EOF
+fi
